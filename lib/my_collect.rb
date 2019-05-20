4bbbfc3ceba63a7942b01461615b9c4c1baed6ed
Alexander Riccio <test35965@gmail.com>
2019-05-20 17:27:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   iter = 0
   new_collection = []
   while iter < collection.size() do
-    yield collection[iter]
+    new_collection << yield collection[iter]
     iter += 1
   end
   new_collection
