db8f33386923e29ab2185ed044b02b68be1f4326
Alexander Riccio <test35965@gmail.com>
2019-05-20 17:28:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(collection)
   iter = 0
   new_collection = []
-  collection.each do |item
-    new_collection << yield collection[iter]
-    iter += 1
+  collection.each do |item|
+    new_collection << yield item
+    
   end
   new_collection
 end
